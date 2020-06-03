4b58a5f43c7fca4dd3f9ed687b670652ec0b06da
rolando-R <landon3388@gmail.com>
2020-06-03 18:18:03 -0500

    agregando  depemdencia react laziload a carousel

--- src/components/carousel/setCarousel.js
+++ src/components/carousel/setCarousel.js
@@ -1,20 +1,24 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
 import { Carousel } from "react-bootstrap";
 import carousel01 from "../../img/carousel/carousel01.jpg";
 import carousel02 from "../../img/carousel/carousel02.jpg";
 import carousel03 from "../../img/carousel/carousel03.jpg";
 import carousel04 from "../../img/carousel/carousel04.jpg";
-import "./setCarousel.scss"
+import "./setCarousel.scss";
 
 export default function setCarousel() {
   return (
-    <Carousel fade >
-      <Carousel.Item >
-        <img
-          className="d-block w-100"
-          src={carousel01}
-          alt="First slide"
-        />
+    <Carousel fade>
+      <Carousel.Item>
+        <LazyLoad throttle={200} height={300}>
+          <img
+            //loading="lazy"
+            className="d-block w-100"
+            src={carousel01}
+            alt="First slide"
+          />
+        </LazyLoad>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -22,7 +26,7 @@ export default function setCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          loading="lazy"
+         // loading="lazy"
           className="d-block w-100"
           src={carousel02}
           alt="Third slide"
@@ -35,7 +39,7 @@ export default function setCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          loading="lazy"
+          // loading="lazy"
           className="d-block w-100"
           src={carousel03}
           alt="Third slide"
@@ -50,7 +54,7 @@ export default function setCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          loading="lazy"
+          //loading="lazy"
           className="d-block w-100"
           src={carousel04}
           alt="Third slide"
